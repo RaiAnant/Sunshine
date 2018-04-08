5281f45f1850ec1d0d51d659a3d5f9428b058dea
Anant <007anantrai@gmail.com>
2018-04-08 17:10:01 +0530

    commit

---
+++ app/src/androidTest/java/com/example/anantrai/sunshine/app/MainActivityFragmentTest.java
@@ -0,0 +1,8 @@
+package com.example.anantrai.sunshine.app;
+
+/**
+ * Created by Anant Rai on 16-01-2018.
+ */
+public class MainActivityFragmentTest {
+
+}
---
+++ app/src/main/java/com/example/anantrai/sunshine/app/BlankFragment.java
@@ -0,0 +1,76 @@
+package com.example.anantrai.sunshine.app;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    private ShareActionProvider shareActionProvider;
+    private String forecast;
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    private Intent createForecastIntent(){
+        Intent shareIntent=new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,forecast);
+        return shareIntent;  
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detail_fragment,menu);
+        MenuItem item=menu.findItem(R.id.share_action);
+        shareActionProvider=(ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        if(shareActionProvider!=null){
+            shareActionProvider.setShareIntent(createForecastIntent());
+        }
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.share_action){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_blank, container, false);
+        TextView textView=(TextView)view.findViewById(R.id.forecast);
+        forecast=getArguments().getString("weather");
+        textView.setText(forecast);
+        return view;
+    }
+
+}
